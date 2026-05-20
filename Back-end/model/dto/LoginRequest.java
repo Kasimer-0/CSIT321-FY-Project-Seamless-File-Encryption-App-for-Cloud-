aff953f9d27ada8434458ba17a84c4692ebdb878
Kasimer-0 <127816217+Kasimer-0@users.noreply.github.com>
2026-05-20 21:35:59 +0800

    Delete Back-end/model directory

--- Back-end/model/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.stealthsync.model.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String usernameOrEmail; // usernameOrEmail for front-end integration
-    private String password;        // password for front-end integration
-}
